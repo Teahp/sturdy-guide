57e2fd64db09dac3f1012beb7a7092d2e405b4b6
wangjiaqiang01 <3350256003@qq.com>
2026-08-15 17:56:15 +0800

    feat:新增demo,向用户问好

---
+++ include/sturdy_guide/demo.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+
+namespace sturdy_guide {
+
+    std::string user_greeting();
+
+}  // namespace sturdy_guide
---
+++ src/demo.cpp
@@ -0,0 +1,9 @@
+#include "sturdy_guide/demo.hpp"
+
+namespace sturdy_guide {
+
+std::string user_greeting() {
+  return "Hello, wjq!"; 
+}
+
+}  // namespace sturdy_guide
